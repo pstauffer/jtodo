abd895594ebaa55bc977eac1faffce29171e89b2
pascal <pascal>
2012-11-13 17:28:35 +0100

    hibernate classes and hbm files added

---
+++ src/main/java/ch/zhaw/jtodo/common/ReminderHome.java
@@ -0,0 +1,134 @@
+package ch.zhaw.jtodo.common;
+// default package
+// Generated Nov 13, 2012 5:11:22 PM by Hibernate Tools 3.1.0.beta4
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+
+
+/**
+ * Home object for domain model class Reminder.
+ * @see .Reminder
+ * @author Hibernate Tools
+ */
+public class ReminderHome {
+
+    private static final Log log = LogFactory.getLog(ReminderHome.class);
+
+    private final SessionFactory sessionFactory = getSessionFactory();
+    
+    protected SessionFactory getSessionFactory() {
+        try {
+            return (SessionFactory) new InitialContext().lookup("SessionFactory");
+        }
+        catch (Exception e) {
+            log.error("Could not locate SessionFactory in JNDI", e);
+            throw new IllegalStateException("Could not locate SessionFactory in JNDI");
+        }
+    }
+    
+    public void persist(Reminder transientInstance) {
+        log.debug("persisting Reminder instance");
+        try {
+            sessionFactory.getCurrentSession().persist(transientInstance);
+            log.debug("persist successful");
+        }
+        catch (RuntimeException re) {
+            log.error("persist failed", re);
+            throw re;
+        }
+    }
+    
+    public void attachDirty(Reminder instance) {
+        log.debug("attaching dirty Reminder instance");
+        try {
+            sessionFactory.getCurrentSession().saveOrUpdate(instance);
+            log.debug("attach successful");
+        }
+        catch (RuntimeException re) {
+            log.error("attach failed", re);
+            throw re;
+        }
+    }
+    
+    public void attachClean(Reminder instance) {
+        log.debug("attaching clean Reminder instance");
+        try {
+            sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+            log.debug("attach successful");
+        }
+        catch (RuntimeException re) {
+            log.error("attach failed", re);
+            throw re;
+        }
+    }
+    
+    public void delete(Reminder persistentInstance) {
+        log.debug("deleting Reminder instance");
+        try {
+            sessionFactory.getCurrentSession().delete(persistentInstance);
+            log.debug("delete successful");
+        }
+        catch (RuntimeException re) {
+            log.error("delete failed", re);
+            throw re;
+        }
+    }
+    
+    public Reminder merge(Reminder detachedInstance) {
+        log.debug("merging Reminder instance");
+        try {
+            Reminder result = (Reminder) sessionFactory.getCurrentSession()
+                    .merge(detachedInstance);
+            log.debug("merge successful");
+            return result;
+        }
+        catch (RuntimeException re) {
+            log.error("merge failed", re);
+            throw re;
+        }
+    }
+    
+    public Reminder findById( int id) {
+        log.debug("getting Reminder instance with id: " + id);
+        try {
+            Reminder instance = (Reminder) sessionFactory.getCurrentSession()
+                    .get("Reminder", id);
+            if (instance==null) {
+                log.debug("get successful, no instance found");
+            }
+            else {
+                log.debug("get successful, instance found");
+            }
+            return instance;
+        }
+        catch (RuntimeException re) {
+            log.error("get failed", re);
+            throw re;
+        }
+    }
+    
+    
+    public List findByExample(Reminder instance) {
+        log.debug("finding Reminder instance by example");
+        try {
+            List results = sessionFactory.getCurrentSession()
+                    .createCriteria("Reminder")
+                    .add(Example.create(instance))
+            .list();
+            log.debug("find by example successful, result size: " + results.size());
+            return results;
+        }
+        catch (RuntimeException re) {
+            log.error("find by example failed", re);
+            throw re;
+        }
+    } 
+
+}
