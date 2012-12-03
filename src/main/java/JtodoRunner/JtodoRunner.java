70ca7c012b9d206c76ed33688f0634e6e1ef1338
pascal <pascal>
2012-12-04 00:13:18 +0100

    blubb

---
+++ src/main/java/JtodoRunner/JtodoRunner.java
@@ -0,0 +1,15 @@
+package JtodoRunner;
+
+import ch.zhaw.jtodo.controller.GUIController;
+
+public class JtodoRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// new CRUDService().testCrudService();
+		new GUIController();
+	}
+
+}
